ba5d722a81316d171d1a06e9e7be70c417ad419d
Scott  Keplinger <skeplin17@gmail.com>
2020-05-23 18:25:31 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,4 +3,13 @@ module Findable
   def find_by_name(name)
     self.all.detect{|a| a.name == name}
   end
+
+  def self.find_or_create_by_name(name)
+     if self.find_by_name(name) != nil
+        self.find_by_name(name)
+     else
+       song = self.create(name)
+       song
+     end
+    end
 end
